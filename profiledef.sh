653c1e96b96e088e98280d2b9c3ebb8255c363db
demonkillerr <gaurangvishwakarma619@gmail.com>
2024-02-03 00:44:22 +0530

    update iso-name

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="alg-cinnamon"
+iso_name="alg-cinnamon-pure"
 iso_label="ALG_CIN_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="DemonKiller <https://demonkiller.tech>"
 iso_application="ALG Live/Rescue CD"
